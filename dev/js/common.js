5179c2ae31674e78ab217551df7781f2c2f1ee9b
kaipeng <417940197@qq.com>
2016-11-05 12:01:47 +0800

    做到新闻了

--- dev/js/common.js
+++ dev/js/common.js
@@ -1,21 +1,23 @@
 /**
  * Created by Administrator on 2016/11/2.
  */
-$(function(){
-    var nav = $(".nav");
+$(function () {
+    var ind = 0;
+    var nav = $(".nav", "#navBar");
     var init = $(".nav .m").eq(ind);
     var block = $(".nav .block");
     block.css({
         "left": init.position().left - 3
     });
-    nav.hover(function() {},
-        function() {
+    nav.hover(function () {
+        },
+        function () {
             block.stop().animate({
                     "left": init.position().left - 3
                 },
                 300);
         });
-    $(".nav").slide({
+    $(nav).slide({
         type: "menu",
         titCell: ".m",
         targetCell: ".sub",
@@ -23,7 +25,7 @@ $(function(){
         triggerTime: 0,
         returnDefault: true,
         defaultIndex: ind,
-        startFun: function(i, c, s, tit) {
+        startFun: function (i, c, s, tit) {
             block.stop().animate({
                     "left": tit.eq(i).position().left - 3
                 },
@@ -32,9 +34,21 @@ $(function(){
     });
 });
 
-var ind = 0;
-//
-myFocus.set({
-    id:'myFocus',//ID
-    pattern:'mF_quwan'//
+
+// 菜单固定
+$(function () {
+    //获取要定位元素距离浏览器顶部的距离;
+    var header = document.querySelector('#header');
+    var navH = $(header).offset().top;
+    //滚动条事件
+    $(window).scroll(function () {
+        //获取滚动条的滑动距离
+        var scroH = $(this).scrollTop();
+        //滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
+        if (scroH >= navH) {
+            $(header).css({"position": "fixed"});
+        } else if (scroH < navH) {
+            $(header).css({"position": "relative"});
+        }
+    })
 });
--- dev/js/pages/index.js
+++ dev/js/pages/index.js
@@ -2,44 +2,10 @@
  * Created by Administrator on 2016/11/2.
  */
 
-$(function () {
-    var nav = $(".nav", "#header");
-    var current = 0;
-    var init = $(".m", "#navBar").eq(current);
-    var block = $("#current", "#navBar");
-    block.css({
-        "left": init.position().left - 3
-    });
-    nav.hover(function () {
-        },
-        function () {
-            block.stop().animate({
-                    "left": init.position().left - 3
-                },
-                400);
-        });
-    $(".nav", "#header").slide({
-        type: "menu",
-        titCell: ".m",
-        targetCell: ".sub",
-        delayTime: 400,
-        triggerTime: 0,
-        returnDefault: true,
-        defaultIndex: current,
-        startFun: function (i, c, s, tit) {
-            block.stop().animate({
-                    "left": tit.eq(i).position().left - 3
-                },
-                300);
-        }
-    });
-});
-
-
+//图片展示
 $(function () {
     var arrLiBox = $('.project-list', '#project');
     var arrLi = arrLiBox.find('li');
-
     $.each(arrLi, function () {
         $(this).mouseover(function () {
             var mask = $(this).find('.mask');
@@ -53,7 +19,6 @@ $(function () {
 });
 
 //轮播图
-
 $(function () {
     //初始化
     var size = $("li", ".img").size();  //获取图片的个数
